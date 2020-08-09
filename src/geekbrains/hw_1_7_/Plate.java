7d2a717388aa8cbac56736560f38578d70b46475
Chobotock <ilya_ageev@bk.ru>
2020-08-09 22:49:29 +0300

    hw_1_7_

--- src/geekbrains/hw_1_7_/Plate.java
+++ src/geekbrains/hw_1_7_/Plate.java
@@ -1,24 +1,54 @@
 package geekbrains.hw_1_7_;
 
-public class Plate {
-    private int food;
+import java.util.Scanner;
 
-    public Plate(int food) {
+public class Plate {
+    private double food;
+
+    public Plate(double food) {
         this.food = food;
     }
 
-    public int getFood() {
-        return food;
+    public boolean catFull (double amount) {
+        return food >= amount;
     }
 
-    public void decreaseFood(int amount) {
-        food -= amount;
+    public void decreaseFood(double amount) {
+        if (food >= amount) {
+            food -= amount;
+            System.out.println("is full");
+        } else if (food > 0 && food < amount) {
+            double fullness = (food / amount);
+            food -= food;
+            System.out.println("isn't fully fed ");
+            System.out.printf("Fullness is %.2f from 1\n", fullness);
+            System.out.println("\nAdd food? Insert 1 for add or another key for exit: ");
+            Scanner sc = new Scanner(System.in);
+            int userVar = sc.nextInt();
+            if (userVar == 1) {
+                System.out.println("Add food: ");
+                Scanner sc1 = new Scanner(System.in);
+                int plusFood = sc1.nextInt();
+                food += plusFood;
+            }
+        } else {
+            System.out.println("is hungry. Plate is empty");
+            System.out.println("\nAdd food? Insert 1 for add or another key for exit: ");
+            Scanner sc = new Scanner(System.in);
+            int userVar = sc.nextInt();
+            if (userVar == 1) {
+                System.out.println("Add food: ");
+                Scanner sc1 = new Scanner(System.in);
+                int plusFood = sc1.nextInt();
+                food += plusFood;
+            }
+
+        }
     }
 
     @Override
     public String toString() {
-        return "Plate{" +
-                "food=" + food +
-                '}';
+        return "\nPlate {" + "food left = " + food + "}";
     }
+
 }
